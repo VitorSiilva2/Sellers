d113a6a0fd641380062b00bcdb69841883d5674a
VitorSiilva2 <vitoralves450@gmail.com>
2023-06-03 10:00:41 -0300

    Class Seller

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,17 @@
 package org.example;
 
 import org.example.model.entities.Department;
+import org.example.model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department objDepartment = new Department(1, "books");
 
-        System.out.println(objDepartment);
+        Seller objSeller = new Seller(20, "Bob", "bob@gmail.com", new Date(), 3000.0, objDepartment );
+
+
+        System.out.println(objSeller);
     }
 }
