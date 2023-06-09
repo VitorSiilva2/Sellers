aea56eb9781840309001999afc343e0f94d407fd
VitorSiilva2 <vitoralves450@gmail.com>
2023-06-09 06:20:48 -0300

    Subindo novamente DB Exception e conexão com DB

---
+++ src/main/java/org/example/db/DB.java
@@ -0,0 +1,71 @@
+package org.example.db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class DB {
+    private static Connection conn = null;
+
+    //Metodo para abrir uma conexão
+    public static Connection getConnection() throws DbException {
+        if (conn == null) {
+            try {
+                Properties props = loadProperties();
+                String url = props.getProperty("dburl");
+                conn = DriverManager.getConnection(url, props);
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    //Metodo para fechar a conexão
+    public static void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+
+            }
+            catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+
+    private static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        }
+        catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeStatement(Statement st){
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+    public static void closeResultSet(ResultSet rs){
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+}
